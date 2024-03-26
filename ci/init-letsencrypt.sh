ff8e6bcf757a5e8bc78c10276d8fcf30cb666be8
Demidov Evgeniy <demidov.e@citilink.ru>
2024-03-26 20:58:11 +0300

    commit

--- ci/init-letsencrypt.sh
+++ ci/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(eternalblueyoga.ru www.eternalblueyoga.ru)
+domains=(eternalblueyoga.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dmitrydemidov48@gmail.com" # Adding a valid address is strongly recommended
